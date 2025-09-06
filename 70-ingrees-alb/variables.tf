40ac7854bdb2a83ed97169eb398ec09c5fcb1264
Gopi <gopikuchi1827@gmail.com>
2025-09-06 12:00:39 +0530

    k8s

---
+++ 70-ingrees-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z07819026HFJ4AW1UBNN"
+}
+
+variable "zone_name" {
+    default = "gopinath.store"
+}
