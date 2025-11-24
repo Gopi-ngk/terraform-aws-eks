19228d451f05dcd7a3a0f0071ee60b333b2df1e4
Gopi <gopikuchi1827@gmail.com>
2025-11-24 21:35:42 +0530

    Jenkins

--- 80-eks/main.tf
+++ 80-eks/main.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 21.0" # this is module version
 
   name               = "${var.project}-${var.environment}"
-  kubernetes_version = "1.32"
+  kubernetes_version = "1.33"
 
   addons = {
     coredns                = {}
@@ -34,21 +34,21 @@ module "eks" {
 
   # EKS Managed Node Group(s)
   eks_managed_node_groups = {
-    blue = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2023_x86_64_STANDARD" # user name is ec2-user
-      instance_types = ["m5.xlarge"]
+    # blue = {
+    #   # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+    #   ami_type       = "AL2023_x86_64_STANDARD" # user name is ec2-user
+    #   instance_types = ["m5.xlarge"]
       
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
+    #   min_size     = 2
+    #   max_size     = 10
+    #   desired_size = 2
 
-       iam_role_additional_policies = {
-        AmazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-        AmazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-        AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
-       }
-    }
+      #  iam_role_additional_policies = {
+      #   AmazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      #   AmazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+      #   AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
+      #  }
+    # }
     # iam_role_additional_policies = {
     #     AmazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
     #     AmazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
